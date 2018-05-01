1b48aac93a5ff05b39146963e40f072fa3b62898
Jonatan Van Hove <>
2018-05-01 16:51:44 +0000

    [auto commit by joon]

--- scriptsTemplate/deploy.sh
+++ scriptsTemplate/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash  
+cd ..
 git pull
 git add .
+got add -A
 git commit -a -m "[auto commit by joon]"
 git push
-lftp -e "mirror -R ../htdocs htdocs && exit" -u USERNAME,PASSWORD FTPSERVER
+lftp -e "mirror -R htdocs htdocs && exit" -u USERNAME,PASSWORD FTPSERVER
